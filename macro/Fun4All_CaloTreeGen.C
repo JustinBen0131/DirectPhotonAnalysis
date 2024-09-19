ec27b1fe27c453e65df9cfb36cc53521b20917e0
JustinBen0131 <146116947+JustinBen0131@users.noreply.github.com>
2024-09-19 00:19:10 -0500

    Update Fun4All_CaloTreeGen.C

--- macro/Fun4All_CaloTreeGen.C
+++ macro/Fun4All_CaloTreeGen.C
@@ -13,47 +13,48 @@
 #include <phool/recoConsts.h>
 
 #include <calotreegen/caloTreeGen.h>
+#include <ClusterIso.h>
 
 R__LOAD_LIBRARY(libfun4all.so)
 R__LOAD_LIBRARY(libffarawobjects.so)
 R__LOAD_LIBRARY(libcaloTreeGen.so)
+R__LOAD_LIBRARY(libcalo_reco.so)
+R__LOAD_LIBRARY(libclusteriso.so)
+
+
 #endif
 
-void Fun4All_CaloTreeGen(const int nEvents = 0, const char *listFile = "fileList_withGeo_timingCut_Template.list", const char *inName = "commissioning.root")
+void Fun4All_CaloTreeGen(const int nEvents = 0, const char *listFile = "/sphenix/lustre01/sphnxpro/physics/slurp/caloy2calib/ana430_2024p007/run_00047100_00047200/DST_CALO_run2pp_ana430_2024p007-00047114-00456.root", const char *inName = "commissioning.root")
 {
     std::cout << "working ... " << std::endl;
   Fun4AllServer *se = Fun4AllServer::instance();
   recoConsts *rc = recoConsts::instance();
 
   gSystem->Load("libg4dst");
+    
+  std::cout << "Creating caloTreeGen module..." << std::endl;
 
   caloTreeGen *calo = new caloTreeGen(inName);
+
   //What subsystems do you want?
   calo -> doEMCal(1,"TOWERINFO_CALIB_CEMC");
-  //Store EMCal clusters?
-  calo -> doClusters(1,"CLUSTERINFO_CEMC");
-
-  //Store tower information for each EMCal cluster?
-  calo -> doClusterDetails(1);
-  
   //Store HCal information?
   calo -> doHCals(1,"TOWERINFO_CALIB_HCALOUT","TOWERINFO_CALIB_HCALIN");
-
+  //Store EMCal clusters?
+  calo -> doClusters(1,"CLUSTERINFO_CEMC");
+  //Store tower information for each EMCal cluster?
+  calo -> doClusterDetails(1);
   //Store ZDC information?
   calo -> doZDC(1,"TOWERINFO_CALIB_ZDC");
-
-  //Store GL1 Information?
-  calo -> doTrig(0,"GL1Packet");
-  calo -> doTrig(1,"GL1Packet");
-  calo -> doTrig(2,"GL1Packet");
-  
+    
   
   se->registerSubsystem(calo);
 
+
   Fun4AllInputManager *in = new Fun4AllDstInputManager("DSTcalo");
+
   
   in->AddFile(listFile);    // condor
-  //in->AddListFile(listFile, 1);      // local
 
   se->registerInputManager(in);
 
