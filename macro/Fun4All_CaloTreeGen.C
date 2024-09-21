0ff697e582df3412c21a4af4e1f0a4cde2a5de9b
JustinBen0131 <146116947+JustinBen0131@users.noreply.github.com>
2024-09-21 15:48:40 -0500

    Update Fun4All_CaloTreeGen.C

--- macro/Fun4All_CaloTreeGen.C
+++ macro/Fun4All_CaloTreeGen.C
@@ -24,40 +24,20 @@ R__LOAD_LIBRARY(libclusteriso.so)
 
 #endif
 
-void Fun4All_CaloTreeGen(const int nEvents = 0, const char *listFile = "/sphenix/lustre01/sphnxpro/physics/slurp/caloy2calib/ana430_2024p007/run_00047100_00047200/DST_CALO_run2pp_ana430_2024p007-00047114-00456.root", const char *inName = "commissioning.root")
-{
-    std::cout << "working ... " << std::endl;
+void Fun4All_CaloTreeGen(const int nEvents = 0, const char *listFile = "/sphenix/lustre01/sphnxpro/physics/slurp/caloy2calib/ana430_2024p007/run_00048600_00048700/DST_CALO_run2pp_ana430_2024p007-00048657-003.root", const char *inName = "commissioning.root") {
   Fun4AllServer *se = Fun4AllServer::instance();
   recoConsts *rc = recoConsts::instance();
 
   gSystem->Load("libg4dst");
     
-  std::cout << "Creating caloTreeGen module..." << std::endl;
-
   caloTreeGen *calo = new caloTreeGen(inName);
-
-  //What subsystems do you want?
-  calo -> doEMCal(1,"TOWERINFO_CALIB_CEMC");
-  //Store HCal information?
-  calo -> doHCals(1,"TOWERINFO_CALIB_HCALOUT","TOWERINFO_CALIB_HCALIN");
-  //Store EMCal clusters?
-  calo -> doClusters(1,"CLUSTERINFO_CEMC");
-  //Store tower information for each EMCal cluster?
-  calo -> doClusterDetails(1);
-  //Store ZDC information?
-  calo -> doZDC(1,"TOWERINFO_CALIB_ZDC");
     
-  
   se->registerSubsystem(calo);
-
 
   Fun4AllInputManager *in = new Fun4AllDstInputManager("DSTcalo");
 
-  
   in->AddFile(listFile);    // condor
-
   se->registerInputManager(in);
-
   se->run(nEvents);
   se->End();
   se->PrintTimer();
