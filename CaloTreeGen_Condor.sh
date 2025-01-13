88eb41b31750047325d272bcb770fc89a33b51df
JustinBen0131 <146116947+JustinBen0131@users.noreply.github.com>
2025-01-13 13:16:08 -0600

    Update CaloTreeGen_Condor.sh

--- CaloTreeGen_Condor.sh
+++ CaloTreeGen_Condor.sh
@@ -13,22 +13,42 @@ fileList=$2
 clusterID=$3
 events=0  # Default number of events
 
-# Create a directory based on the run number
-outputDir="/sphenix/tg/tg01/bulk/jbennett/DirectPhotons/output/${runNumber}"
-mkdir -p ${outputDir}
 
-# Check if the file list is provided
-if [ ! -f "$fileList" ]; then
-  echo "[ERROR] File list not provided or does not exist: $fileList"
-  exit 1
+
+if [ "$runNumber" == "SIMMODE" ]; then
+  echo "[INFO] Detected simulation mode."
+  # e.g., no runNumber needed
+  # we can define a special output directory for sim
+  outputDir="/sphenix/tg/tg01/bulk/jbennett/DirectPhotons/output/SimOut"
+  mkdir -p ${outputDir}
+
+  # The file list for sim is presumably a single file with 1 line
+  mapfile -t rootFiles < "$fileList"
+  fileBaseName=$(basename "${rootFiles[0]}")
+  treeOutName="${outputDir}/CaloTreeGenSim_${fileBaseName%.*}.root"
+
+  # Here we rely on the macro being set to wantSim = true and wantData = false
+  root -b -l -q "macro/Fun4All_CaloTreeGen.C(0, \"$fileList\", \"$treeOutName\")"
+
+else
+    echo "[INFO] Detected data mode. runNumber = $runNumber"
+    # Create a directory based on the run number
+    outputDir="/sphenix/tg/tg01/bulk/jbennett/DirectPhotons/output/${runNumber}"
+    mkdir -p ${outputDir}
+
+    # Check if the file list is provided
+    if [ ! -f "$fileList" ]; then
+      echo "[ERROR] File list not provided or does not exist: $fileList"
+      exit 1
+    fi
+
+    # Read the list of files from the text file
+    mapfile -t rootFiles < "$fileList"
+
+    # Construct the output filename using the first file in the list
+    fileBaseName=$(basename "${rootFiles[0]}")
+    treeOutName="${outputDir}/CaloTreeGen_${fileBaseName%.*}.root"
+
+    # Run the ROOT macro with the list of input files
+    root -b -l -q "macro/Fun4All_CaloTreeGen.C(0, \"$fileList\", \"$treeOutName\")"
 fi
-
-# Read the list of files from the text file
-mapfile -t rootFiles < "$fileList"
-
-# Construct the output filename using the first file in the list
-fileBaseName=$(basename "${rootFiles[0]}")
-treeOutName="${outputDir}/CaloTreeGen_${fileBaseName%.*}.root"
-
-# Run the ROOT macro with the list of input files
-root -b -l -q "macro/Fun4All_CaloTreeGen.C(0, \"$fileList\", \"$treeOutName\")"
