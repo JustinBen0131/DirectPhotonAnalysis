e338f30daba622211542d1850dd9d2ce63405969
JustinBen0131 <146116947+JustinBen0131@users.noreply.github.com>
2025-01-25 16:14:08 -0600

    Update runCondor_hAddRunByRunSegments.sh

--- macro/runCondor_hAddRunByRunSegments.sh
+++ macro/runCondor_hAddRunByRunSegments.sh
@@ -1,34 +1,46 @@
 #!/usr/bin/env bash
 
-# Read run numbers from the input file
-inputFile="/sphenix/user/patsfan753/tutorials/tutorials/CaloDataAnaRun24pp/runListTriggerLUTv1.txt"
-
-# Read run numbers into an array
-mapfile -t runNumbers < ${inputFile}
-
 # Ensure directories are set up
-mkdir -p /tmp/patsfan753_condor_logs
-mkdir -p /sphenix/user/patsfan753/tutorials/tutorials/CaloDataAnaRun24pp/stdout
-mkdir -p /sphenix/user/patsfan753/tutorials/tutorials/CaloDataAnaRun24pp/error
+mkdir -p /tmp/${USER}_condor_logs
+mkdir -p /sphenix/user/${USER}/tutorials/tutorials/CaloDataAnaRun24pp/stdout
+mkdir -p /sphenix/user/${USER}/tutorials/tutorials/CaloDataAnaRun24pp/error
+
+# Read run numbers from the input file
+inputFile="/sphenix/user/${USER}/tutorials/tutorials/CaloDataAnaRun24pp/Final_RunNumbers_After_All_Cuts.txt"
+mapfile -t runNumbers < ${inputFile}
 
 # Check if 'local' is passed as an argument
 if [ "$1" == "local" ]; then
-    echo "Running locally for testing purposes."
+    if [ -z "$2" ]; then
+        echo "Please provide a run number or a file containing run numbers for local processing."
+        exit 1
+    fi
 
-    testRunNumber=${runNumbers[0]}
-
-    # Execute the processing script locally
-    ./mergeSegmentFilesForRuns.sh ${testRunNumber}
+    if [ -f "$2" ]; then
+        # The second argument is a file containing run numbers
+        mapfile -t localRunNumbers < "$2"
+        echo "Running locally on run numbers from file ${2}."
+        for runNumber in "${localRunNumbers[@]}"; do
+            echo "Processing run number ${runNumber} locally."
+            ./mergeSegmentFilesForRuns.sh ${runNumber}
+        done
+    else
+        # The second argument is a single run number
+        testRunNumber=$2
+        echo "Running locally for testing purposes on run number ${testRunNumber}."
+        # Execute the processing script locally
+        ./mergeSegmentFilesForRuns.sh ${testRunNumber}
+    fi
 else
-    # Loop over each run number and create custom submission files
+    # Existing functionality: Submit jobs to Condor
     for runNumber in "${runNumbers[@]}"; do
         cat > condor_${runNumber}.submit <<EOL
 universe                = vanilla
 executable              = mergeSegmentFilesForRuns.sh
 arguments               = ${runNumber}
-log                     = /tmp/patsfan753_condor_logs/job.\$(Cluster).\$(Process).log
-output                  = /sphenix/user/patsfan753/tutorials/tutorials/CaloDataAnaRun24pp/stdout/job.\$(Cluster).\$(Process).out
-error                   = /sphenix/user/patsfan753/tutorials/tutorials/CaloDataAnaRun24pp/error/job.\$(Cluster).\$(Process).err
+log                     = /tmp/${USER}_condor_logs/job.\$(Cluster).\$(Process).log
+output                  = /sphenix/user/${USER}/tutorials/tutorials/CaloDataAnaRun24pp/stdout/job.\$(Cluster).\$(Process).out
+error                   = /sphenix/user/${USER}/tutorials/tutorials/CaloDataAnaRun24pp/error/job.\$(Cluster).\$(Process).err
 request_memory          = 1.5GB
 PeriodicHold            = (NumJobStarts>=1 && JobStatus == 1)
 concurrency_limits      = CONCURRENCY_LIMIT_DEFAULT:100
@@ -39,4 +51,3 @@ EOL
         condor_submit condor_${runNumber}.submit
     done
 fi
-
